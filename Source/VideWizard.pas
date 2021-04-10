a63d931d83e7115e50f99001f5921bc9a0307308
Kai Anter <kai.anter@web.de>
2021-04-10 21:42:06 +0200

    Added: License note to source files

--- Source/VideWizard.pas
+++ Source/VideWizard.pas
@@ -1,3 +1,23 @@
+{
+  This file registers the ViDelphi Wizard in the Delphi IDE.
+
+  Copyright (c) 2016 Peter Ross
+  Copyright (C) 2021  Kai Anter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+}
+
 unit VideWizard;
 
 interface
